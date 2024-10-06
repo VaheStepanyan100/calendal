c810e34b9f3c71b0ed4e343a4c9d40e7252ff4d2
VaheStepanyan100 <vahestepanyan100@gmail.com>
2024-10-06 13:32:11 +0400

    Init Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
-  return null;
+  return (
+    <div className="text-center container my-4 mx-auto">
+      <h1 className="text-3xl mb-4">Fancy Home Page</h1>
+      <div className="flex gap-2 justify-center">
+        <Button>Sign In</Button>
+        <Button>Sign Out</Button>
+      </div>
+    </div>
+  )
 }
